ed4bd0b041292f51d432ee661cbd6f1201dc8acc
Jonnattan Griffiths <jonnattan@gmail.com>
2025-10-17 20:57:25 +0000

    Se modifica el cron para que sea más top

--- cron/verify_news.sh
+++ cron/verify_news.sh
@@ -2,5 +2,5 @@
 
 cd /home/jonnattan/source/email-api/cron
 echo "[INFO] --------- $(date): Consultando si existen novedades" > ./logs/verify_news.sh.log
-curl --location -X POST -H 'Content-Type: application/json' -H 'Authorization: Basic Y2hlY2s6e3t2YXVsdDphdXRob3JpemF0aW9uLXBhc3N3b3JkfX0=' -H 'x-api-key: cbd68de2-0f2c-44ac-9646-707245c48263' -d '{"type":"clear","data":{"name":"Jonna"}}' 'https://api.jonnattan.cl/email/news' >> ./logs/verify_news.sh.log
+curl --location -X POST -H 'Content-Type: application/json' -H 'Authorization: Basic __BASIC_VALUE__' -H 'x-api-key: __API_KEY__' -d '{"type":"clear","data":{"name":"Jonna"}}' 'https://api.jonnattan.cl/email/news' >> ./logs/verify_news.sh.log
 echo "[INFO] --------- $(date): Consulta realizada" > ./logs/verify_news.sh.log
